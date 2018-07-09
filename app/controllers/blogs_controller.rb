15a5234911d3f1e8952fd69915960dfaa4c04d66
Katsuhiro Suzuki <katsuhiro@MacBookPro.local>
2018-07-09 21:53:28 +0900

    アソシエーション課題

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:edit, :update, :destroy]
+  before_action :set_blog, only: [:edit, :update, :destroy, :show]
   before_action :logged_in?, only: [:new, :edit, :show, :destroy]
 
   def index
@@ -16,6 +16,7 @@ class BlogsController < ApplicationController
 
   def create
     @blog = Blog.new(blog_params)
+    @blog.user_id = current_user.id
     if @blog.save
       redirect_to blogs_path
     else
@@ -24,7 +25,9 @@ class BlogsController < ApplicationController
   end
 
   def confirm
-    @blog = Blog.new(blog_params)
+    # @blog = Blog.new(blog_params)
+    # @blog.user_id = current_user.id
+    @blog = current_user.blogs.build(blog_params)
     render :new if @blog.invalid?
   end
 
@@ -48,7 +51,10 @@ class BlogsController < ApplicationController
     unless current_user.present?
       redirect_to new_session_path
     end
+  end
 
+  def show
+    @favorite = current_user.favorites.find_by(blog_id: @blog.id)
   end
 
   private
