bf75cdd0ef6f9039c58bf93c40ef1e917b4f9e7b
Katsuhiro Suzuki <katsuhiro@MacBookPro.local>
2018-07-10 18:38:44 +0900

    if文削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @favorites = current_user.favorites
+    @favorites = current_user.try(:favorites)
   end
 
   private
