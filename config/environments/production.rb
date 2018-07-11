54b5190a4f57f3817ad39d93491afbd6a26c52ed
Katsuhiro <katsuhiro@MacBookPro.local>
2018-07-11 20:21:55 +0900

    herokuで画像投稿時に、デリバリーエラーが発生したので、deliver_methodを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'enigmatic-falls-57466.herokuapp.com' }
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :letter_opener_web
 end
